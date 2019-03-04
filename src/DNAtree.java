cb5747129be31b7cbde7ab81164623295bd6a392
Joey Rodgers <jdr14@vt.edu>
2019-03-04 15:43:39 -0500

    Finished adding the skeleton classes for all of the types.  This includes the type interface from which the three inherit.  Started on Parse and created an Enum in the main class (containing the main loop

--- src/DNAtree.java
+++ src/DNAtree.java
@@ -1,9 +1,36 @@
 
+/**
+ * Main/Central class containing the main loop
+ * @author Joey Rogders jdr14
+ * @author Jovany Cabrera jovanyc4
+ * @version 1.0.0
+ */
 public class DNAtree {
-
+	
+	/**
+	 * Enum to list the types which will define the nodes 
+	 * stored in the DNA tree.
+	 * @author Joey Rodgers
+	 * @version 1.0.0
+	 */
+    private static enum Types
+    {
+        INTERNAL,
+        EMPTY,
+        DNATYPE;
+    }
+    
+    /**
+     * Main loop for the DNA Node Tree project (i.e. project 2)
+     * Ties the project functionality together
+     * @author: Joey Rogders jdr14
+     * @author: Jovany Cabrera jovanyc4
+     * @version 1.0.0
+     * @param args
+     */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        
 	}
-
-}
+    
+} 
