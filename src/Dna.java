0f9605d8573ad1b2214c3f90793a46460dbceeb4
jovany <jovanyc4@vt.edu>
2019-03-05 12:48:16 -0500

    possibly finished the Node class file. Might have to make modifications. Skeleton of DNA class.

---
+++ src/Dna.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+/**
+ * 
+ * @author Jovany Cabrera jovanyc4
+ * @author Joey Rodgers
+ * @version 1
+ *
+ */
+
+public class Dna{
+	
+	/**
+	 * Default Constructor
+	 */
+	public Dna()
+	{
+		
+	}
+}
