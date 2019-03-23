5a037c0686c35b087264a81b2b9c408b3b3a713b
Joey Rodgers <jdr14@vt.edu>
2019-03-23 12:02:20 -0400

    Added another class for testing the DNA tree.

--- src/dnatreeTest.java
+++ src/dnatreeTest.java
@@ -59,6 +59,25 @@ public class dnatreeTest extends TestCase
     	assertEquals(dnaType.isDNA(), true);
     	assertEquals(dnaType.getCommand(), "insert");
     	assertEquals(dnaType.getSequence(), "ACAA");
+        
+        // Test calculations for percentages of each letter in the sequence
+        assertEquals(dnaType.getPercentA(), 75.0);
+        assertEquals(dnaType.getPercentC(), 25.0);
+        assertEquals(dnaType.getPercentG(), 0.00);
+        assertEquals(dnaType.getPercentT(), 0.00);
+        
+        // Test the set command method
+        dnaType.setCommand("remove");
+        assertEquals(dnaType.getCommand(), "remove");
+
+
+
+    }
+    
+    // Use this to test the Dna.java class containing the tree methods
+    public void testDna()
+    {
+
     }
 }
 
